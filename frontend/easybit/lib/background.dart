8bca22d323bc8fc26983973ebef92da98f2152e1
Assurance Tedangbe Kakiang <tedangbek@gmail.com>
2023-11-09 16:14:31 +0000

    flutter project creation

--- frontend/easybit/lib/background.dart
+++ frontend/easybit/lib/background.dart
@@ -6,35 +6,16 @@ class Background extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
-        alignment: Alignment.center,
-        height: size.height * 1,
-        width: size.width * 1,
-        decoration: BoxDecoration(
-          color: Colors.white70,
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: const [
-            BoxShadow(
-                color: Colors.black26, blurRadius: 6, offset: Offset(0, 2))
-          ],
-          //border: Border.all(color: Colors.blue, width: 3)
-        ),
-        child: const Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            DecoratedBox(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("images/background.png"),
-                    fit: BoxFit.cover),
-              ),
-              child: Center(
-                  child: FlutterLogo(
-                size: 200,
-              )),
-            ),
-          ],
-        ));
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("images/background.png"), fit: BoxFit.cover),
+      ),
+      child: Center(
+          child: FlutterLogo(
+        size: 200,
+      )),
+    );
   }
 }
 
