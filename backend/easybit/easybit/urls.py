bc54e844716bded852511f6f3681164dd87f16d6
Isabelle Olive KANTOUSSAN <isabelle.kantoussan@atos.net>
2023-11-23 10:46:51 +0000

    API pour l'achat et la vente de bitcoins

--- backend/easybit/easybit/urls.py
+++ backend/easybit/easybit/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from authentication.register import register
 from converting_calculator.converting import Convert_XOF , Convert_BTC
 from authentication.signing import login_view, logout_view
+from Transactions.Achat_Vente import vente_bitcoin_api, confirmation_vente_api, achat_bitcoin_api,confirmation_achat_api
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -28,5 +30,9 @@ urlpatterns = [
     path("login/", login_view),
     path("logout/", logout_view),
     path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
-   
+    path('achat_bitcoin/', achat_bitcoin_api, name= "achat"),
+    path ("vente_bitcoin/", vente_bitcoin_api , name = "vente"),
+    path("confirmation_vente/", confirmation_vente_api, name='confirmation_vente'),
+    path("confirmation_achat/", confirmation_achat_api, name='confirmation_achat'),
+    
 ]
